67d0d0e65f0d24c8d3e3e8803e3ca2d5fdb17582
devcarlosperez <carlosperezsantana04@gmail.com>
2025-04-28 23:14:20 +0100

    added library jtattoo

--- src/es/daw/carlos/biblioteca/Biblioteca.java
+++ src/es/daw/carlos/biblioteca/Biblioteca.java
@@ -1,20 +1,23 @@
 package es.daw.carlos.biblioteca;
 
+import com.jtattoo.plaf.bernstein.BernsteinLookAndFeel;
 import es.daw.carlos.biblioteca.config.ConexionBD;
 import es.daw.carlos.biblioteca.vista.BibliotecaApp;
-
 import java.sql.*;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
 public class Biblioteca {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedLookAndFeelException {
         Connection conn = ConexionBD.conectar();
         if (conn == null) {
             System.out.println("Error al conectar la base de datos");
         } else {
             ConexionBD.crearTablas(conn);
         }
-        
+        UIManager.setLookAndFeel(new NimbusLookAndFeel());
         new BibliotecaApp();
     }
 }
